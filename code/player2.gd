5c4f3fbf2430b64f35aeabf18e3de0e8e4a9b274
Emily <130906954+em54art@users.noreply.github.com>
2023-09-20 17:54:59 +1000

    add animation tree
    animations added to movement and attack (not completed still has to do idle and walk equiped)

--- code/player2.gd
+++ code/player2.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 
-#@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var animation_tree : AnimationTree = $AnimationTree
 var direction : Vector2 = Vector2.ZERO
 
-#func _ready():
-#	animation_tree.active = true
+func _ready():
+	animation_tree.active = true
 	
-#func _process(delta):
-#	update_animation_parameters()
+func _process(delta):
+	update_animation_parameters()
 
 func _physics_process(delta):
 #move 4 directions
@@ -23,22 +23,22 @@ func _physics_process(delta):
 	move_and_slide()
 
 #animations
-#func update_animation_parameters():
-#	if(velocity == Vector2.ZERO):
-#		animation_tree["parameters/conditions/idle"] = true
-#		animation_tree["parameters/conditions/is_moving"] = false
-#	else:
-#		animation_tree["parameters/conditions/idle"] = false
-#		animation_tree["parameters/conditions/is_moving"] = true
-#
-#	if(Input.is_action_just_pressed("use")):
-#		animation_tree["parameters/conditions/attack"] = true
-#	else:
-#		animation_tree["parameters/conditions/attack"] = false
-#
-#	if(direction != Vector2.ZERO):
-#		animation_tree["parameters/idle/blend_position"] = direction
-#		animation_tree["parameters/walk/blend_position"] = direction
-#		animation_tree["parameters/water_atk/blend_position"] = direction
+func update_animation_parameters():
+	if(velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+
+	if(Input.is_action_just_pressed("use")):
+		animation_tree["parameters/conditions/attack"] = true
+	else:
+		animation_tree["parameters/conditions/attack"] = false
+
+	if(direction != Vector2.ZERO):
+		animation_tree["parameters/idle/blend_position"] = direction
+		animation_tree["parameters/walk/blend_position"] = direction
+		animation_tree["parameters/water_atk/blend_position"] = direction
 
 
