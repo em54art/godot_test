b39676630cefe216c16c9aa40c5221c04a658faf
Emily <130906954+em54art@users.noreply.github.com>
2023-09-23 15:46:33 +1000

    add objects, camera
    added camera to follow the player
    added rock and plant to the island

--- code/EquipSprite.gd
+++ code/EquipSprite.gd
@@ -2,10 +2,3 @@ extends Node2D
 
 @export var equiped_item : Resource
 
-#test
-func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if body.has_method("rock_touch"):
-		print("nice")
-	else:
-		print("bad")
-		pass
